c5d95761ef0adedcc857f39f1fdf4044eb53638f
Rachel Michaela Bradley <45085543+racheldelevingne@users.noreply.github.com>
2020-11-03 15:31:41 -0600

    Refactor the demo content into multiple functions.

--- www/main.js
+++ www/main.js
@@ -9,40 +9,64 @@ function show(elementID) {
 function begin() {
 	hide("start");
 	show("picker");
-	picker();
 }
 
-var licenses = [
+function end() {
+	hide("yes");
+	hide("no");
+}
+
+function restartbtn() {
+	location.reload(true);
+}
+
+var licenses = new Map([
 	["BSD 2-Clause License", 0],
 	["GNU Affero General Public License, version 3.0", 0],
 	["Copyright, no free software license", 0]
-];
+]);
 
-function picker() {
-	document.getElementById("yes").addEventListener("click", function() {
-		console.log("Software project selected.");
-		setQuestion("Should your users have the right to modify your source code?");
-		document.getElementById("yes").addEventListener("click", function() {
-			console.log("Free software selected.");
-			setQuestion("If a user wants to use your code in their own project, should they be required to share their project's source code with their users?");
-			document.getElementById("yes").addEventListener("click", function() {
-				console.log("Mandatory sharing selected.");
+var stages = new Map([
+	["Are you licensing a software project?", null],
+	["Should users be able to modify your source code?", null],
+	["Should users be required to distribute their modifications?", null]
+]);
+
+function weight(license) {
+	licenses.set(license, licenses.get(license)++);
+}
+
+function response(boolean) {
+	if (boolean == true) {
+		stages.set(document.getElementById("question").textContent, 1);
+	} else {
+		stages.set(document.getElementById("question").textContent, 0);
+	}
+	progress();
+}
+
+function progress() {
+	if (stages.get("Are you licensing a software project?") != 1) {
+		setQuestion("Project licenser does not support creative projects at this time.")
+		end();
+	} else {
+		if(stages.get("Should users be able to modify your source code?") == null) {
+			setQuestion("Should users be able to modify your source code?");
+		} else if (stages.get("Should users be able to modify your source code?") == 1) {
+			if(stages.get("Should users be required to distribute their modifcations?") == null) {
+				setQuestion("Should users be required to distribute their modifcations?");
+			} else if (stages.get("Should users be required to distribute their modifcations?") == 1) {
 				setQuestion("We recommend the GNU General Public License, version 3.0 for your project.");
-			});
-			document.getElementById("no").addEventListener("click", function() {
-				console.log("Voluntary sharing selected.");
+				end();
+			} else {
 				setQuestion("We recommend the BSD 2-Clause License for your project.");
-			});
-		});
-		document.getElementById("no").addEventListener("click", function() {
-			console.log("Proprietary software selected.");
+				end();
+			}
+		} else {
 			setQuestion("We recommend standard copyright with no free software license for your project.");
-		});
-	});
-	document.getElementById("no").addEventListener("click", function() {
-		console.log("Creative project selected.");
-		setQuestion("Project licenser does not support creative projects at this time.");
-	})
+			end();
+		}
+	}
 }
 
 function setQuestion(questionText) {
