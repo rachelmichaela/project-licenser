2f087dad45a9a45211daf8ca1e9996389de73a14
Rachel Michaela Bradley <45085543+racheldelevingne@users.noreply.github.com>
2020-11-17 08:24:18 -0600

    Implement creative licenses branch.

--- www/main.js
+++ www/main.js
@@ -44,18 +44,19 @@ var softwareLicenses = [
 ];
 
 var creativeLicenses = [
-	["Copyright, no license", 0],
-	["Creative Commons Attribution 4.0", 0],
-	["Creative Commons Attribution No-Derivatives 4.0", 0],
-	["Creative Commons Attribution Non-Commercial 4.0", 0],
-	["Creative Commons Attribution Non-Commercial No-Derivatives 4.0", 0],
-	["Creative Commons Attribution Non-Commercial Share-Alike 4.0", 0],
-	["Creative Commons Attribution Share-Alike 4.0", 0],
-	["FreeBSD Documentation License", 0],
-	["GNU Free Documentation License", 0],
-	["GNU Verbatim Copyright License", 0],
-	["Public Domain", 0],
-	["SIL Open Font License", 0]
+	["CC0", 0], // 0
+	["Copyright, no license", 0], // 1
+	["Creative Commons Attribution 4.0", 0], // 2
+	["Creative Commons Attribution No-Derivatives 4.0", 0], // 3
+	["Creative Commons Attribution Non-Commercial 4.0", 0], // 4
+	["Creative Commons Attribution Non-Commercial No-Derivatives 4.0", 0], // 5
+	["Creative Commons Attribution Non-Commercial Share-Alike 4.0", 0], // 6
+	["Creative Commons Attribution Share-Alike 4.0", 0], // 7
+	["FreeBSD Documentation License", 0], // 8
+	["GNU Free Documentation License", 0], // 9
+	["GNU Verbatim Copyright License", 0], // 10
+	["Public Domain", 0], // 11
+	["SIL Open Font License", 0] // 12
 ];
 
 function weight(licenses) {
@@ -72,7 +73,7 @@ function weight(licenses) {
 
 var stages = [
 	["Are you contributing to an existing project?", -1, 1, null, null], // existing project license, next question
-	["Are you licensing a software project?", 2, 0, null, null], // software branch, creative branch
+	["Are you licensing a software project?", 2, 14, null, null], // software branch, creative branch
 	["Should users be able to modify your source code?", 3, 0, [1, 2, 3, 4, 6, 7, 8, 9, 10, 11, 12], [5]], // free software branch, copyright
 	["Should users be required to distribute their modifications?", 4, 8, [4, 6, 8, 9, 11], [0, 1, 2, 3, 7, 10, 12]], // copyleft branch, permissive branch
 	["Should users be able to run the project on web servers without sharing source code?", 5, 0, [4, 8, 9, 11], [6]], // next question, AGPL
@@ -84,7 +85,20 @@ var stages = [
 	["Do you want to relinquish all copyright rights?", 0, 11, [12], [1, 2, 7, 10]], // Public domain, next question
 	["Do you need to be able to disallow certain users or use cases from your project?", 0, 12, [10], [1, 2, 7]], // ISC, next question
 	["Should users be able to promote their project by referencing your own without written permission?", 0, 13, [1, 7], [2]], // next question, BSD 3-Clause
-	["Would you prefer a license authored by the Free Software Foundation?", 0, 0, [7], [1]] // GNU APL, BSD 2-Clause
+	["Would you prefer a license authored by the Free Software Foundation?", 0, 0, [7], [1]], // GNU APL, BSD 2-Clause
+	["Should users be allowed to modify your work?", 15, 0, [0, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], [1]], // free culture branch, copyright
+	["Do you want to relinquish all copyright rights?", 16, 17, [0, 11], [2, 3, 4, 5, 6, 7, 8, 9, 10, 12]], // public domain options, licensed options
+	["Do you want a formal license to declare your relinquishment?", 0, 0, [0], [11]], // CC0, public domain
+	["Are you licensing written work?", 18, 20, null, null], // documentation, testimony
+	["Are you licensing documentation?", 19, 20, [8, 9], [2, 3, 4, 5, 6, 7, 10, 12]], // documentation, testimony
+	["Should users be required to distribute any modifications that they make?", 0, 0, [9], [8]], // GNU FDL, FreeBSD Doc license
+	["Are you licensing testimony, opinion articles, or other documents that should be free to share but not change?", 0, 21, [10], [2, 3, 4, 5, 6, 7, 12]], // GNU Verbatim, font next question
+	["Are you licensing a font?", 0, 22, [12], [2, 3, 4, 5, 6, 7]], // SIL, next question
+	["Should users be allowed to profit from your project?", 23, 25, [2, 3, 7], [4, 5, 6]], // CC, CC NonCom
+	["Should users be allowed to create derivative works based on your project?", 24, 0, [2, 7], [3]],
+	["Should users be required to share derivative works under the same license?", 0, 0, [7], [2]],
+	["Should users be allowed to create derivative works based on your project?", 26, 0, [4, 6], [5]],
+	["Should users be required to share derivative works under the same license?", 0, 0, [6], [4]]
 ];
 
 var step = 0;
